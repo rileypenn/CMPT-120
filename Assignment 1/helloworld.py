b9837739a0b552250e45d01ccbe70b7512be42de
rileypenn <112398021+rileypenn@users.noreply.github.com>
2022-09-11 14:46:37 -0400

    WOOOO2

--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -7,10 +7,16 @@
 def main():
 
 
+    # instructions: print out "Hello World!"
+    print("Hello World!")
 
-    
-    
-    
-    
+    #create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is:
+    favoriteColor = "Green"
+    print("My favorite color is" , favoriteColor)
+
+    #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
+    birthday = 322004
+    print("My birthday is" , birthday)
+
     
 main()
