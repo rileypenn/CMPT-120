7017aa3ec2fcd4f86417cc0d303e6701e13392fc
rileypenn <112398021+rileypenn@users.noreply.github.com>
2022-10-19 16:28:56 -0400

    WOOOOOOOOOOOOOO

--- Assignment 3/Debug.py
+++ Assignment 3/Debug.py
@@ -1,11 +1,14 @@
 def main():
     
     #Can you print out "Hello" 8 times? I gave you a tiny hint to start...
-    for x in range(?):
+    for x in range(8):
+        print("hello")
         
     #What about as a while loop?
-    loops = ?
-    while (loops < ?):
+    loops = 0
+    while (loops < 9):
+        print ("Hello")
+        loops = loops + 1
         
     
     #The loop iterations are one behind in a non-programming counting way... how can we fix this?
@@ -17,34 +20,39 @@ def main():
     #Same deal here!
     for x in range(3):
         print("For loop iteration:", x)
+        count = count + 1
      
     #Uh oh I messed up and made an infinite loop... so silly of me!   
     endless = 4    
     while (endless < 5):
         print("I'm stuck in this loop!!!!")
-    
-    #print out your last name one letter at a time
-    for x in "":
-        print(?)
-       
-     #aw i suck i made another infinite loop.. use that thing I taught you about to get out once it prints once... starts with a b... br....
-    found == False    
+        endless = endless + 1
+
+    # print out your last name one letter at a time
+    name = ["Penn"]
+    for x in name:
+        print(x)
+
+    # aw i suck i made another infinite loop.. use that thing I taught you about to get out once it prints once... starts with a b... br....
+    found == False
     while found == False:
         print("i only printed once")
-        
-    #can you fill this out so that it prints found when it hits the letter r?
+        break
+
+    # can you fill this out so that it prints found when it hits the letter r?
     for x in "Marist":
-        if ? == ?:
+        if "Marist" == "r":
             print("found")
-    
-    numbers = [1,2,3,4,5,6,7,8,9,10]
-    #you could print out the list using print(numbers) OR you could go the long way and use a for loop to print out the value of each index :)
-    for x in ?:
-        print(?[?])
-    
-    #what if I wanted you to print out only the even numbers in this range I made?
-    for x in range (20, 501):
-        #i feeeeel like modulooooooo is neededddd
-        if
-    
+
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    # you could print out the list using print(numbers) OR you could go the long way and use a for loop to print out the value of each index :)
+    for x in numbers:
+        print(numbers[x-1])
+
+    # what if I wanted you to print out only the even numbers in this range I made?
+    for x in range(20, 501):
+        # i feeeeel like modulooooooo is neededddd
+        if x%2 == 0:
+            print(x)
+
 main()
