49fe02440e9bbb3f921cee75379d311d6802f5b4
rileypenn <112398021+rileypenn@users.noreply.github.com>
2022-12-05 08:05:02 -0500

    Update Debug.py

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -1,38 +1,58 @@
 def main():
-    #good working example!
+   # good working example!
     stringInput = input("enter a string")
     if stringInput.isalpha():
         print("string!")
     else:
         print("not string :(")
-        
-    #can you google and find what function you should use to check if it's numeric (an int?)?
+
+    # can you google and find what function you should use to check if it's numeric (an int?)?
     intInput = input("enter an int")
-    if intInput.?:
+    if intInput.isnumeric():
         print("int!")
     else:
         print("not int :(")
-    
-    #what about if it's both letters and numbers?
+
+
+    # what about if it's both letters and numbers?
     alphIntInput = input("Enter letters and numbers")
-    if alphIntInput.?:
+    if alphIntInput.isalnum():
         print("Letters and numbers!")
     else:
         print("weird characters :(")
-       
-    #good working example
+
+    # good working example
     asterisk = input("Enter an asterisk please")
     if asterisk == "*":
         print("good!")
     else:
         print("not asterisk :(")
-        
-    #now write code to check if the input was either an asterisk OR an ampersand (&)
-    
-        
-    #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
-    
+
+    # now write code to check if the input was either an asterisk OR an ampersand (&)
+    asterAmperInput = input("Enter an asterisk or an ampersand")
+    if asterAmperInput == "*":
+        print("Asterisk")
+    elif asterAmperInput == "&":
+        print("Ampersand")
+    else:
+        print("smh follow directions")
+
+
+    # do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
+    integerInput = input("enter an integer")
+    if integerInput.isnumeric():
+        print("yes")
+    else:
+        print("no")
+
     # last challenge: find out how to check if the string input has the substring "marist"
-    #google this one ;) substring is the key google term
-    
+    # google this one ;) substring is the key google term
+    maristString= "marist"
+   
+    if "need" in maristString:
+        print("Yes! it is present in the string")
+    else:
+        print("No! it is not present")
+
+
 main()
