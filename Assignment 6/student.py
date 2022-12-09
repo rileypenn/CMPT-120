e4a5b714635acf6ed0125b9b875af42de9e7a425
rileypenn <112398021+rileypenn@users.noreply.github.com>
2022-12-09 08:35:16 -0500

    Update student.py

--- Assignment 6/student.py
+++ Assignment 6/student.py
@@ -1,4 +1,4 @@
-#oo scary
+# oo scary
 '''
 Instructions:
 We're now experts at classes, right?
@@ -16,7 +16,7 @@ create a function to see if the student is eligible for the honors program
 to be eligible they need to have a gpa above 3.5
 return true if they can and false if they cant, and print it out
 
-create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student, that student gets free food that day. 
+create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student, that student gets free food that day.
 1. generate a random number the length of however long you choose to make the id number
 2. compare if the random number matches your student's id
 3. if it matches print out "winner! student (name) gets free lunch!"
@@ -24,15 +24,63 @@ create a function because this college is a wacky one- every day they generate a
 (disclosure: obviously there's a very small chance of your generated number matching the student id number. I just want to see that you're generating and comparing properly)
 '''
 
+
 class student:
-    
-    
-    
-    
-    
-    
+    def __init__(self, name, studentId, year, major, gpa):
+        #name
+        self.name = name
+        #studentId
+        self.studentId = studentId
+        #year
+        self.year = year
+        #major
+        self.major = major
+        #gpa
+        self.gpa = gpa
+
+import random
+
+def randN(N):
+	min = pow(10, N-1)
+	max = pow(10, N) - 1
+	return random.randint(min, max)
+
+
 def main():
-    #create three students and check if they get free lunch and if they qualify for honors
+
+# create a function to see if the student is eligible for the honors program
+    def honorsAcceptance(self):
+        # to be eligible they need to have a gpa above 3.5
+        # return true if they can and false if they cant, and print it out
+        if gpa > 3.5:
+            return true
+            print("Congrats, you are accepted!")
+        else:
+            return false
+            print("Sorry, you are rejected")
+
+    #create a function because this college is a wacky one- every day they generate a student id and if the student id matches a student, that student gets free food that day.
+    def freeLunch(self):
+        #1. generate a random number the length of however long you choose to make the id number
+        generatedStudentId = randN(8)
+        print(generatedStudentId)
+
+# create three students and check if they get free lunch and if they qualify for honors
+    student1 = Student(Cally, 20495867, 2001, arts, 3.6)
+    student2 = Student(Michael, 10389246, 2002, buisness, 2.9)
+    student3 = Student(Terek, 69306872, 2000, math, 3.8)
+
+    if freeLunch() == student1:
+        print("Congrats student 1!")
+        
+    elif freeLunch() == student2:
+        print("Congrats student 2")
+        
+    elif freeLunch() == student3:
+        print("Congrats student 3")
+        
+    else:
+        print("No free lunch :(")
     
-    
+
 main()
